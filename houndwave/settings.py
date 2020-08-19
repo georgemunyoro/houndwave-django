5edd249f2cad3dbf0ebeb8945949ae98230b5566
George Munyoro <32019551+georgemunyoro@users.noreply.github.com>
2020-08-19 19:30:30 +0000

    Update settings.py

--- houndwave/settings.py
+++ houndwave/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
